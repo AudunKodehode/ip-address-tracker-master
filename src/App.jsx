4df8b2c73b885a75124b5599b91c3e86418f6e2d
AudunKodehode <audunkodehode@gmail.com>
2023-10-25 17:18:32 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ function App() {
   return (
     <>
       <IPTracker />
+      <div className="fem">
+    Challenge by <a href="https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0">Frontend Mentor.</a> Coded by <a href="https://github.com/AudunKodehode">AudunKodehode</a>. 
+    </div>
     </>
   );
 }
